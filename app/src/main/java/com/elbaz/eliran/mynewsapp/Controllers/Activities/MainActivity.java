4211d69fa56a757e6622430751ca7c2b88cdef92
e210564e <devedelie@gmail.com>
2019-07-06 14:52:08 +0200

    Initial commit

---
+++ app/src/main/java/com/elbaz/eliran/mynewsapp/Controllers/Activities/MainActivity.java
@@ -0,0 +1,34 @@
+package com.elbaz.eliran.mynewsapp.Controllers.Activities;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.elbaz.eliran.mynewsapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Configure ViewPager
+        this.configureViewPagerAndTabs();
+    }
+
+    private void configureViewPagerAndTabs(){
+        //Get ViewPager from layout
+        ViewPager pager = (ViewPager)findViewById(R.id.activity_main_viewpager);
+        //Set Adapter PageAdapter and glue it together
+        pager.setAdapter(new PageAdapter(getSupportFragmentManager()));
+
+        //Get TabLayout from layout
+        TabLayout tabs= (TabLayout)findViewById(R.id.activity_main_tabs);
+        //Glue TabLayout and ViewPager together
+        tabs.setupWithViewPager(pager);
+        //Design purpose. Tabs have the same width
+        tabs.setTabMode(TabLayout.MODE_FIXED);
+    }
+}
+
