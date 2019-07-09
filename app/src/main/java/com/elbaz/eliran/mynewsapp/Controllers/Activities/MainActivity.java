60e2eb74c6488a39393e5a1e9a8ec409b176f30e
e210564e <devedelie@gmail.com>
2019-07-09 13:03:00 +0200

    - Added navigation drawer to the toolbar

--- app/src/main/java/com/elbaz/eliran/mynewsapp/Controllers/Activities/MainActivity.java
+++ app/src/main/java/com/elbaz/eliran/mynewsapp/Controllers/Activities/MainActivity.java
@@ -1,8 +1,13 @@
 package com.elbaz.eliran.mynewsapp.Controllers.Activities;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -12,7 +17,14 @@ import android.widget.Toast;
 import com.elbaz.eliran.mynewsapp.Adapters.PageAdapter;
 import com.elbaz.eliran.mynewsapp.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+
+    // NYT API key
+    protected static final String API_KEY = "eUdpsuImhyQRapDx4vkN0NMOJZEYSqYA";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +34,55 @@ public class MainActivity extends AppCompatActivity {
         //1 - Configuring Toolbar
         this.configureToolbar();
 
-        //Configure ViewPager
+        //2 - Configure ViewPager
         this.configureViewPagerAndTabs();
+
+        //3 - Configure Drawer
+        this.configureDrawerLayout();
+
+        //4 - Configure NavigationView
+        this.configureNavigationView();
 
     }
 
+    /**
+     * Navigation menu items declaration
+     */
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        // 4 - Handle Navigation Item Click
+        int id = menuItem.getItemId();
+
+        switch (id){
+            case R.id.activity_main_drawer_news :
+                break;
+            case R.id.activity_main_drawer_profile:
+                break;
+            case R.id.activity_main_drawer_settings:
+                break;
+            default:
+                break;
+        }
+
+        this.drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+
+    /**
+     * Toolbar execution
+     */
+    private void configureToolbar(){
+        // Get the toolbar view inside the activity layout
+        this.toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
+        // Sets the Toolbar
+        setSupportActionBar(toolbar);
+    }
+
+    /**
+     * ViewPager configuration + Subject Tabs
+     */
     private void configureViewPagerAndTabs(){
         //Get ViewPager from layout
         ViewPager pager = (ViewPager)findViewById(R.id.activity_main_viewpager);
@@ -41,6 +97,9 @@ public class MainActivity extends AppCompatActivity {
         tabs.setTabMode(TabLayout.MODE_FIXED);
     }
 
+    /**
+     * Inflate the menu with the icons (search and more)
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         //2 - Inflate the menu and add it to the Toolbar
@@ -48,6 +107,9 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    /**
+     * Top-Menu icons action
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         //3 - Handle actions on menu items
@@ -63,13 +125,24 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // ----
-
-    private void configureToolbar(){
-        // Get the toolbar view inside the activity layout
-        Toolbar toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
-        // Sets the Toolbar
-        setSupportActionBar(toolbar);
+    /**
+     * Configure Drawer Layout
+     */
+    private void configureDrawerLayout(){
+        this.drawerLayout = (DrawerLayout) findViewById(R.id.activity_main_drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
+
+    /**
+     * Configure NavigationView
+     */
+    private void configureNavigationView(){
+        this.navigationView = (NavigationView) findViewById(R.id.activity_main_nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+
 }
 
