c5000eba1b18f094d92320eba9251471c4f795e7
e210564e <devedelie@gmail.com>
2019-07-23 10:38:17 +0200

    - Created the Search activity and implemented the toolbar

---
+++ app/src/main/java/com/elbaz/eliran/mynewsapp/Controllers/Activities/SearchAndNotificationsActivity.java
@@ -0,0 +1,31 @@
+package com.elbaz.eliran.mynewsapp.Controllers.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.elbaz.eliran.mynewsapp.R;
+
+public class SearchAndNotificationsActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_and_notifications);
+
+        this.configureToolbar();
+    }
+
+    private void configureToolbar(){
+        //Get the toolbar (Serialise)
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Toolbar
+        setSupportActionBar(toolbar);
+        //Get a support ActionBar corresponding to this toolbar
+        ActionBar actionBar = getSupportActionBar();
+        // Enable the upper button (back button)
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+}
