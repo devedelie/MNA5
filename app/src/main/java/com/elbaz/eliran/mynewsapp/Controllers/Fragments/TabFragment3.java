627e227f06e302771b8354973139062fc917a23b
e210564e <devedelie@gmail.com>
2019-07-23 10:45:02 +0200

    - Temporarily duplicated fragments 3 & 4 to test other top stories functionality - Next will be to inherit these fragment views from TabFragment1

--- app/src/main/java/com/elbaz/eliran/mynewsapp/Controllers/Fragments/TabFragment3.java
+++ app/src/main/java/com/elbaz/eliran/mynewsapp/Controllers/Fragments/TabFragment3.java
@@ -2,22 +2,121 @@ package com.elbaz.eliran.mynewsapp.Controllers.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.bumptech.glide.Glide;
+import com.elbaz.eliran.mynewsapp.Models.TopStoriesModels.NYTNews;
+import com.elbaz.eliran.mynewsapp.Models.TopStoriesModels.Result;
 import com.elbaz.eliran.mynewsapp.R;
+import com.elbaz.eliran.mynewsapp.Utils.NYTStreams;
+import com.elbaz.eliran.mynewsapp.Views.NYTAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableObserver;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by Eliran Elbaz on 06-Jul-19.
  */
 public class TabFragment3 extends Fragment {
+    private Disposable mDisposable;
+    private List<Result> mResults;
+    private NYTAdapter mNYTAdapter;
+
+    // try
+    @BindView(R.id.fragment_3_recyclerView)
+    RecyclerView mRecyclerView;
+
+
     public static TabFragment3 newInstance() {
         return (new TabFragment3());
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_tab_3, container, false);
+        View view = inflater.inflate(R.layout.fragment_tab_3, container, false);
+        // Call during UI creation
+        ButterKnife.bind(this, view);
+
+        // Set the recyclerView to fix size in order to increase performances
+        mRecyclerView.setHasFixedSize(true);
+
+        this.configureRecyclerView();
+        this.executeHttpRequestWithRetrofit();
+
+        return view;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        this.disposeWhenDestroy();
+    }
+
+    //-----------------
+    // RecyclerView Config
+    //-----------------
+    private void configureRecyclerView(){
+        mResults = new ArrayList<>();
+        mNYTAdapter = new NYTAdapter(mResults, getContext(), Glide.with(this));
+        mRecyclerView.setAdapter(this.mNYTAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
+    //-----------------
+    // HTTP (RxJAVA)
+    //-----------------
+
+    // 1 - Execute the stream
+    private void executeHttpRequestWithRetrofit(){
+        // 1.2 - Execute the stream subscribing to Observable defined inside NYTStream
+        this.mDisposable = NYTStreams.streamFetchTopStories("technology")
+                .subscribeWith(new DisposableObserver<NYTNews>(){
+
+                    @Override
+                    public void onNext(NYTNews nytNews) {
+                        // 1.3 - Update UI with list of titles
+                        Log.e(TAG, "onNext" );
+                        updateUI(nytNews.getResults());
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e(TAG, "onError: "+ e );
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "onComplete");
+                    }
+                });
+    }
+
+    // This method will be called onDestroy to avoid any risk of memory leaks.
+    private void disposeWhenDestroy(){
+        if (this.mDisposable != null && !this.mDisposable.isDisposed()) this.mDisposable.dispose();
+    }
+
+    //-----------------
+    // Update UI
+    //-----------------
+
+    // 3 - Update UI showing only titles
+    private void updateUI(List<Result> titles){
+        mResults.clear();
+        mResults.addAll(titles);
+        mNYTAdapter.notifyDataSetChanged();
     }
 }
+
--- app/src/main/java/com/elbaz/eliran/mynewsapp/Controllers/Fragments/TabFragment4.java
+++ app/src/main/java/com/elbaz/eliran/mynewsapp/Controllers/Fragments/TabFragment4.java
@@ -2,27 +2,123 @@ package com.elbaz.eliran.mynewsapp.Controllers.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
+import com.bumptech.glide.Glide;
+import com.elbaz.eliran.mynewsapp.Models.TopStoriesModels.NYTNews;
+import com.elbaz.eliran.mynewsapp.Models.TopStoriesModels.Result;
 import com.elbaz.eliran.mynewsapp.R;
+import com.elbaz.eliran.mynewsapp.Utils.NYTStreams;
+import com.elbaz.eliran.mynewsapp.Views.NYTAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableObserver;
+
+import static android.support.constraint.Constraints.TAG;
 
 /**
  * Created by Eliran Elbaz on 06-Jul-19.
  */
 public class TabFragment4 extends Fragment {
+    private Disposable mDisposable;
+    private List<Result> mResults;
+    private NYTAdapter mNYTAdapter;
+
+    TabFragment1 mTabFragment1 = new TabFragment1();
+
+    // try
+    @BindView(R.id.fragment_4_recyclerView)
+    RecyclerView mRecyclerView;
+
+
     public static TabFragment4 newInstance() {
         return (new TabFragment4());
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_tab_4, container, false);
-        LinearLayout linearLayout = (LinearLayout) rootView.findViewById(R.id.fragment_page_4_rootview);
+        View view = inflater.inflate(R.layout.fragment_tab_4, container, false);
+        // Call during UI creation
+        ButterKnife.bind(this, view);
 
-        return rootView;
+        // Set the recyclerView to fix size in order to increase performances
+        mRecyclerView.setHasFixedSize(true);
+
+        this.configureRecyclerView();
+        this.executeHttpRequestWithRetrofit();
+
+        return view;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        this.disposeWhenDestroy();
+    }
+
+    //-----------------
+    // RecyclerView Config
+    //-----------------
+    private void configureRecyclerView(){
+        mResults = new ArrayList<>();
+        mNYTAdapter = new NYTAdapter(mResults, getContext(), Glide.with(this));
+        mRecyclerView.setAdapter(this.mNYTAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
+
+    //-----------------
+    // HTTP (RxJAVA)
+    //-----------------
+
+    // 1 - Execute the stream
+    private void executeHttpRequestWithRetrofit(){
+        // 1.2 - Execute the stream subscribing to Observable defined inside NYTStream
+        this.mDisposable = NYTStreams.streamFetchTopStories("sports")
+                .subscribeWith(new DisposableObserver<NYTNews>(){
+
+                    @Override
+                    public void onNext(NYTNews nytNews) {
+                        // 1.3 - Update UI with list of titles
+                        Log.e(TAG, "onNext" );
+                        updateUI(nytNews.getResults());
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e(TAG, "onError: "+ e );
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "onComplete");
+                    }
+                });
+    }
+
+    // This method will be called onDestroy to avoid any risk of memory leaks.
+    private void disposeWhenDestroy(){
+        if (this.mDisposable != null && !this.mDisposable.isDisposed()) this.mDisposable.dispose();
+    }
+
+    //-----------------
+    // Update UI
+    //-----------------
+
+    // 3 - Update UI showing only titles
+    private void updateUI(List<Result> titles){
+        mResults.clear();
+        mResults.addAll(titles);
+        mNYTAdapter.notifyDataSetChanged();
     }
 }
