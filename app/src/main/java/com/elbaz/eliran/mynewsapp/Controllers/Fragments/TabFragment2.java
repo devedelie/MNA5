0a00a6e07fe6663de476339965a30a8ee4ddc4cb
e210564e <devedelie@gmail.com>
2019-07-23 11:02:14 +0200

    - Implementation of MostPopular API

--- app/src/main/java/com/elbaz/eliran/mynewsapp/Controllers/Fragments/TabFragment2.java
+++ app/src/main/java/com/elbaz/eliran/mynewsapp/Controllers/Fragments/TabFragment2.java
@@ -1,25 +1,43 @@
 package com.elbaz.eliran.mynewsapp.Controllers.Fragments;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import com.elbaz.eliran.mynewsapp.Models.GithubUser;
+import com.bumptech.glide.Glide;
+import com.elbaz.eliran.mynewsapp.Models.MostPopularModels.NYTMostPopular;
+import com.elbaz.eliran.mynewsapp.Models.MostPopularModels.ResultMostPopular;
 import com.elbaz.eliran.mynewsapp.R;
-import com.elbaz.eliran.mynewsapp.Utils.GithubCalls;
+import com.elbaz.eliran.mynewsapp.Utils.NYTStreams;
+import com.elbaz.eliran.mynewsapp.Views.NYTAdapterMostPopular;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableObserver;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by Eliran Elbaz on 06-Jul-19.
  */
-public class TabFragment2 extends Fragment implements GithubCalls.Callbacks{
+public class TabFragment2 extends Fragment {
 
-    TextView mTextView;
+    private Disposable mDisposable;
+    private List<ResultMostPopular> mResultMostPopulars;
+    private NYTAdapterMostPopular mNYTAdapterMostPopular;
+
+    // try
+    @BindView(R.id.fragment_2_recyclerView) RecyclerView mRecyclerView;
+
 
     public static TabFragment2 newInstance() {
         return (new TabFragment2());
@@ -28,54 +46,77 @@ public class TabFragment2 extends Fragment implements GithubCalls.Callbacks{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_tab_2, container, false);
-        mTextView = (TextView) view.findViewById(R.id.github_text);
-        // here I think should come mRecyclerView = (RecyclerView) findView......
+        // Call during UI creation
+        ButterKnife.bind(this, view);
 
+        // Set the recyclerView to fix size in order to increase performances
+        mRecyclerView.setHasFixedSize(true);
 
-        executeHttpRequestWithRetrofit();
+        this.configureRecyclerView();
+        this.executeHttpRequestWithRetrofit();
 
         return view;
-
     }
 
-    // 4 - Execute HTTP request and update UI
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        this.disposeWhenDestroy();
+    }
+
+    //-----------------
+    // RecyclerView Config
+    //-----------------
+    private void configureRecyclerView(){
+        mResultMostPopulars = new ArrayList<>();
+        mNYTAdapterMostPopular = new NYTAdapterMostPopular(this.mResultMostPopulars, getContext(), Glide.with(this));
+        mRecyclerView.setAdapter(this.mNYTAdapterMostPopular);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
+    //-----------------
+    // HTTP (RxJAVA)
+    //-----------------
+
+    // 1 - Execute the stream
     private void executeHttpRequestWithRetrofit(){
-        this.updateUIWhenStartingHTTPRequest();
-        GithubCalls.fetchCategoryOfNews(this, "home.json");
+        // 1.2 - Execute the stream subscribing to Observable defined inside NYTStream
+        this.mDisposable = NYTStreams.streamFetchMostPopular("facebook")
+                .subscribeWith(new DisposableObserver<NYTMostPopular>(){
+
+                    @Override
+                    public void onNext(NYTMostPopular nytMostPopular) {
+                        // 1.3 - Update UI with list of titles
+                        Log.d(TAG, "onNext fragment 2" );
+                        updateUI(nytMostPopular.getResults());
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e(TAG, "onError fragment 2: "+ e );
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "onComplete fragment 2");
+                    }
+                });
     }
 
-    //-----------------
-    // HTTP Request
-    //-----------------
-    @Override
-    public void onResponse(@Nullable List<GithubUser> users) {
-        if (users != null) this.updateUIWithListOfUsers(users);
+    // This method will be called onDestroy to avoid any risk of memory leaks.
+    private void disposeWhenDestroy(){
+        if (this.mDisposable != null && !this.mDisposable.isDisposed()) this.mDisposable.dispose();
     }
-
-    @Override
-    public void onFailure() {
-        this.updateUIWhenStoppingHTTPRequest("Error: Please check your internet connection");
-    }
-    //-----------------
 
     //-----------------
     // Update UI
     //-----------------
 
-    private void updateUIWhenStartingHTTPRequest(){
-        this.mTextView.setText("Downloading...");
-    }
-
-    private void updateUIWhenStoppingHTTPRequest(String response){
-        this.mTextView.setText(response);
-    }
-
-    // 3 - Update UI showing only name of users
-    private void updateUIWithListOfUsers(List<GithubUser> users){
-        StringBuilder stringBuilder = new StringBuilder();
-        for (GithubUser user : users){
-            stringBuilder.append("-"+user.getLogin()+"\n");
-        }
-        updateUIWhenStoppingHTTPRequest(stringBuilder.toString());
+    // 3 - Update UI showing only titles
+    private void updateUI(List<ResultMostPopular> titles){
+        Log.d(TAG, "updateUI: ");
+        mResultMostPopulars.clear();
+        mResultMostPopulars.addAll(titles);
+        mNYTAdapterMostPopular.notifyDataSetChanged();
     }
 }
