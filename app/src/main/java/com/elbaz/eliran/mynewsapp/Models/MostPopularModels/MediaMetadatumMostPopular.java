113b94e60d541ee5629ad7a07179b81b8c49a913
e210564e <devedelie@gmail.com>
2019-07-23 11:00:56 +0200

    - Reorganize the models of TopStories & MostPopular and added objects instead of Integer List, to avoid receiving "null" while calling the API

---
+++ app/src/main/java/com/elbaz/eliran/mynewsapp/Models/MostPopularModels/MediaMetadatumMostPopular.java
@@ -0,0 +1,58 @@
+package com.elbaz.eliran.mynewsapp.Models.MostPopularModels;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Eliran Elbaz on 20-Jul-19.
+ */
+public class MediaMetadatumMostPopular {
+
+    @SerializedName("url")
+    @Expose
+    private String url;
+    @SerializedName("format")
+    @Expose
+    private String format;
+    @SerializedName("height")
+    @Expose
+    private Integer height;
+    @SerializedName("width")
+    @Expose
+    private Integer width;
+
+
+
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+}
