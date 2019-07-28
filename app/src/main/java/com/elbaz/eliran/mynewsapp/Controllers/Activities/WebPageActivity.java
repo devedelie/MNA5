f9255db0172e64308d5b4b8e610dc527d657aa41
e210564e <devedelie@gmail.com>
2019-07-28 16:13:50 +0200

    - Modified WebView class to receive the URL with StringExtra

--- app/src/main/java/com/elbaz/eliran/mynewsapp/Controllers/Activities/WebPageActivity.java
+++ app/src/main/java/com/elbaz/eliran/mynewsapp/Controllers/Activities/WebPageActivity.java
@@ -4,43 +4,44 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
+import com.elbaz.eliran.mynewsapp.Controllers.Fragments.TabFragment1;
 import com.elbaz.eliran.mynewsapp.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class WebPageActivity extends AppCompatActivity {
+    @BindView(R.id.webView)
     WebView mWebView;
-    String webUrl;
+    String mUrl;
 
-    public WebPageActivity(String webUrl) {
-        this.webUrl = webUrl;
-    }
-
-    private class WebViewClient extends android.webkit.WebViewClient{
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            return false;
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_page);
 
+        ButterKnife.bind(this);
+
         // Call the toolbar
         this.configureToolbar();
+        // Configure the webView
+        this.configureWebView();
 
-        // Create the webView
-        mWebView= (WebView) findViewById(R.id.webView);
-        mWebView.getSettings().setJavaScriptEnabled(true);
-        mWebView.getSettings().setLoadWithOverviewMode(true);
-        mWebView.getSettings().setUseWideViewPort(true);
-        mWebView.getSettings().setBuiltInZoomControls(true);
-        mWebView.getSettings().setPluginState(WebSettings.PluginState.ON);
-        mWebView.setWebViewClient(new WebViewClient());
-        mWebView.loadUrl("https://www.google.com");
+
+
+//        // Create the webView
+//        mWebView= (WebView) findViewById(R.id.webView);
+//        mWebView.getSettings().setJavaScriptEnabled(true);
+//        mWebView.getSettings().setLoadWithOverviewMode(true);
+//        mWebView.getSettings().setUseWideViewPort(true);
+//        mWebView.getSettings().setBuiltInZoomControls(true);
+//        mWebView.getSettings().setPluginState(WebSettings.PluginState.ON);
+//        mWebView.setWebViewClient(new WebViewClient());
+//        mWebView.loadUrl("https://www.google.com");
     }
 
     /**
@@ -55,5 +56,11 @@ public class WebPageActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         // Enable the upper button (back button)
         actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void configureWebView(){
+        String url = getIntent().getStringExtra(TabFragment1.BUNDLE_URL);
+        mWebView.loadUrl(url);
+        mWebView.setWebViewClient(new WebViewClient());
     }
 }
