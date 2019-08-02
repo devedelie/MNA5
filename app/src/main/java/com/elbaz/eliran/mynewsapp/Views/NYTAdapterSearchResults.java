009bc32522fff0c9b7a026f9b5bc00416efb8505
e210564e <devedelie@gmail.com>
2019-08-02 12:02:20 +0200

    - Created an RecyclerView Adapter for SearchResults

---
+++ app/src/main/java/com/elbaz/eliran/mynewsapp/Views/NYTAdapterSearchResults.java
@@ -0,0 +1,60 @@
+package com.elbaz.eliran.mynewsapp.Views;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bumptech.glide.RequestManager;
+import com.elbaz.eliran.mynewsapp.Models.SearchModels.Doc;
+import com.elbaz.eliran.mynewsapp.Models.SearchModels.Response;
+import com.elbaz.eliran.mynewsapp.R;
+
+import java.util.List;
+
+/**
+ * Created by Eliran Elbaz on 01-Aug-19.
+ */
+public class NYTAdapterSearchResults extends RecyclerView.Adapter<NYTViewHolder> {
+
+    //Data
+    private List<Response> mResponses;
+    private List<Doc> mDocs;
+    Context mContext;
+    // Glide object
+    private RequestManager glide;
+
+    public NYTAdapterSearchResults(List<Doc> docs, Context context, RequestManager glide) {
+        mDocs = docs;
+        mContext = context;
+        this.glide = glide;
+    }
+
+    @NonNull
+    @Override
+    public NYTViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        View view = inflater.inflate(R.layout.recyclerview_item, viewGroup, false);
+
+        return new NYTViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NYTViewHolder nytViewHolder, int i) {
+        nytViewHolder.updateSearchResultsWithTitles(this.mDocs.get(i), this.glide);
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.mDocs.size();
+    }
+
+//    /**
+//     * A method to receive the correct title URL from its current position on the list
+//     */
+//    public String getUrl(int position) {
+//        return mDocs.get(position).getWebUrl();
+//    }
+}
