ef2e4ec6f360bec3f1ac27d44edaae80072857b0
e210564e <devedelie@gmail.com>
2019-08-08 00:26:56 +0200

    - implemented the notificationChannel into the Worker class

--- app/src/main/java/com/elbaz/eliran/mynewsapp/Utils/NotificationWorker.java
+++ app/src/main/java/com/elbaz/eliran/mynewsapp/Utils/NotificationWorker.java
@@ -1,10 +1,16 @@
 package com.elbaz.eliran.mynewsapp.Utils;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.v4.app.NotificationCompat;
 
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
+
+import com.elbaz.eliran.mynewsapp.R;
 
 /**
  * Created by Eliran Elbaz on 06-Aug-19.
@@ -19,15 +25,30 @@ public class NotificationWorker extends Worker {
 
     @Override
     public Result doWork() {
-        // Do the work here
-
-        //The Result returned from doWork() informs WorkManager whether the task:
-//        finished successfully via Result.success()
-//        failed via Result.failure()
-//        needs to be retried at a later time via Result.retry()
+        // The work
+        displayNotification("Title here", "Description will be here bla bla...");
 
         // Indicate whether the task finished successfully with the Result
         return Result.success();
+    }
+
+    // A method to display the news result with a daily notification
+    private void displayNotification(String task, String description){
+
+        // 1 - Create a notificationManager in order to create notification channel
+        NotificationManager manager = (NotificationManager) getApplicationContext()
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+        // 2 - If the version is >= from version O, then we create a notification channel
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("dailyNotification", "notification", NotificationManager.IMPORTANCE_DEFAULT);
+            manager.createNotificationChannel(channel);
+        }
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), "dailyNotification")
+                .setContentTitle(task)
+                .setContentText(description)
+                .setSmallIcon(R.mipmap.ic_launcher);
+
+        manager.notify(1, builder.build());
     }
 
 }
