ede1d4e3973777f4c2118e6c9dec6ccf8c3af2ba
e210564e <devedelie@gmail.com>
2019-08-17 18:34:19 +0200

    - Modified instances

--- app/src/test/java/com/elbaz/eliran/mynewsapp/Controllers/Fragments/FragmentsTest.java
+++ app/src/test/java/com/elbaz/eliran/mynewsapp/Controllers/Fragments/FragmentsTest.java
@@ -11,29 +11,25 @@ import static org.junit.Assert.assertNotNull;
  */
 public class FragmentsTest {
 
-    /**
-     * Test to verify that the fragments are not returning null
-     * @throws Exception
-     */
-
+    //Test to verify that the fragments are not returning null
     @Test
     public void Fragment1ShouldNotBeNull() throws Exception {
-        Fragment Fragment1 = TabFragment1.newInstance();
+        Fragment Fragment1 = new TabFragment1();
         assertNotNull(Fragment1);
     }
     @Test
     public void Fragment2ShouldNotBeNull() throws Exception {
-        Fragment Fragment2 = TabFragment2.newInstance();
+        Fragment Fragment2 = new TabFragment2();
         assertNotNull(Fragment2);
     }
     @Test
     public void Fragment3ShouldNotBeNull() throws Exception {
-        Fragment Fragment3 = TabFragment3.newInstance();
+        Fragment Fragment3 = new TabFragment3();
         assertNotNull(Fragment3);
     }
     @Test
     public void Fragment4ShouldNotBeNull() throws Exception {
-        Fragment Fragment4 = TabFragment4.newInstance();
+        Fragment Fragment4 = new TabFragment4();
         assertNotNull(Fragment4);
     }
 
