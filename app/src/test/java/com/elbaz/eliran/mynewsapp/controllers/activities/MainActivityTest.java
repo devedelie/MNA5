920d9ac0395d6967d2a4cae2c47620d338c28787
e210564e <devedelie@gmail.com>
2019-08-19 15:14:31 +0200

    - Added new logo and refactored name

--- app/src/test/java/com/elbaz/eliran/mynewsapp/controllers/activities/MainActivityTest.java
+++ app/src/test/java/com/elbaz/eliran/mynewsapp/controllers/activities/MainActivityTest.java
@@ -1,10 +1,10 @@
 package com.elbaz.eliran.mynewsapp.controllers.activities;
 
-import com.google.android.material.navigation.NavigationView;
 import androidx.viewpager.widget.ViewPager;
 
-import com.elbaz.eliran.mynewsapp.adapters.PageAdapter;
 import com.elbaz.eliran.mynewsapp.R;
+import com.elbaz.eliran.mynewsapp.adapters.PageAdapter;
+import com.google.android.material.navigation.NavigationView;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -12,36 +12,50 @@ import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 
-import static junit.framework.Assert.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Eliran Elbaz on 10-Jul-19.
  */
 
 @RunWith(RobolectricTestRunner.class)
+//@RunWith(MockitoJUnitRunner.class)
 public class MainActivityTest {
-
     private MainActivity mActivity;
     private ViewPager mViewPager;
     private PageAdapter mPageAdapter;
     private NavigationView mNavigationView;
 
+    String[] dialogTextArray;
+
+    private MainActivity activity;
+
     @Before
     public void setUp() throws Exception {
-        mActivity = Robolectric.setupActivity(MainActivity.class);
-        mViewPager = mActivity.findViewById(R.id.activity_main_viewpager);
-        mPageAdapter = (PageAdapter)mViewPager.getAdapter();
+        activity = Robolectric.buildActivity( MainActivity.class )
+                .create()
+                .resume()
+                .get();
+    }
+
+    @Test
+    public void shouldNotBeNull() throws Exception
+    {
+        assertNotNull( activity );
     }
 
     @Test
     public void checkIfNavigationDrawerContainsItem() throws Exception {
+        mActivity = Robolectric.setupActivity(MainActivity.class);
+        mViewPager = mActivity.findViewById(R.id.activity_main_viewpager);
+        mPageAdapter = (PageAdapter)mViewPager.getAdapter();
+
+
         mNavigationView = mActivity.findViewById(R.id.activity_main_nav_view);
         assertEquals("Top Stories", mNavigationView.getMenu().findItem(R.id.activity_main_drawer_1).getTitle().toString() );
         assertEquals("Notifications", mNavigationView.getMenu().findItem(R.id.activity_main_drawer_2).getTitle().toString() );
         assertEquals("bla bla", mNavigationView.getMenu().findItem(R.id.activity_main_drawer_3).getTitle().toString());
     }
-
-
-
 
 }
