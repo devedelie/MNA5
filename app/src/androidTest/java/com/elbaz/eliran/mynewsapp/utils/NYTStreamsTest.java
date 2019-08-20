ed3e9ab06acd0d7005f7117db7c3381158599ca9
e210564e <devedelie@gmail.com>
2019-08-20 16:03:15 +0200

    - Added more stream tests

--- app/src/androidTest/java/com/elbaz/eliran/mynewsapp/utils/NYTStreamsTest.java
+++ app/src/androidTest/java/com/elbaz/eliran/mynewsapp/utils/NYTStreamsTest.java
@@ -1,6 +1,10 @@
 package com.elbaz.eliran.mynewsapp.utils;
 
+import android.util.Log;
+
 import com.elbaz.eliran.mynewsapp.models.MostPopularModels.NYTMostPopular;
+import com.elbaz.eliran.mynewsapp.models.MostPopularModels.ResultMostPopular;
+import com.elbaz.eliran.mynewsapp.models.SearchModels.Doc;
 import com.elbaz.eliran.mynewsapp.models.SearchModels.NYTSearch;
 import com.elbaz.eliran.mynewsapp.models.TopStoriesModels.NYTNews;
 
@@ -9,8 +13,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
 import io.reactivex.Observable;
 import io.reactivex.observers.TestObserver;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by Eliran Elbaz on 17-Aug-19.
@@ -34,8 +47,8 @@ public class NYTStreamsTest {
     }
 
     @Test
-    public void isStream_fetchMostPopularStream_correctSizeReturned() {
-        Observable<NYTMostPopular> observableMostPopular = NYTStreams.streamFetchMostPopular(7);
+    public void isStream_fetchMostPopularLast7Days_ReturnBiggerThanZero() {
+        Observable<NYTMostPopular> observableMostPopular = NYTStreams.streamFetchMostPopular("7"); // Only the following values are allowed: 1, 7, 30
 
         TestObserver<NYTMostPopular> mostPopularTestObserver = new TestObserver<>();
 
@@ -49,8 +62,31 @@ public class NYTStreamsTest {
     }
 
     @Test
-    public void isStream_fetchSearchResultsStream_correctSizeReturned() {
-        Observable<NYTSearch> observableSearchResults = NYTStreams.streamFetchSearchResults(null,null,"politics","","newest");
+    public void isStream_fetchMostPopularLast7Days_ReturnResultsWithinTheLastWeek() throws Exception{
+        Observable<NYTMostPopular> observableMostPopular = NYTStreams.streamFetchMostPopular("7"); // Only the following values are allowed: 1, 7, 30
+
+        TestObserver<NYTMostPopular> mostPopularTestObserver = new TestObserver<>();
+
+        observableMostPopular.subscribeWith(mostPopularTestObserver)
+                .assertNoErrors()
+                .assertNoTimeout()
+                .awaitTerminalEvent();
+
+        NYTMostPopular mostPopular = mostPopularTestObserver.values().get(0);
+        // Assert that all the titles are within the correct period (0-10 days-old max)
+        String dateBefore7Days = subtractDays(10); // the period value 7 returns news within the last 10 days approximetely
+        for (ResultMostPopular result: mostPopular.getResults()){
+            String date = convertDate(result.getPublishedDate());
+            Log.d(TAG, "isStream_ReturnResultsWithinTheLastWeek: " +dateBefore7Days + " "+ date);
+            // check if the date is
+            Assert.assertTrue(Integer.parseInt(date) >= Integer.parseInt(dateBefore7Days));
+
+        }
+    }
+
+    @Test
+    public void isStream_fetchSearchResultsForCategory_ReturnCorrectCategoryWithCorrectSize() {
+        Observable<NYTSearch> observableSearchResults = NYTStreams.streamFetchSearchResults(null,null,"news_desk:(\"Politics\")","","newest");
 
         TestObserver<NYTSearch> searchResultTestObserver = new TestObserver<>();
 
@@ -60,6 +96,82 @@ public class NYTStreamsTest {
                 .awaitTerminalEvent();
 
         NYTSearch searchResults = searchResultTestObserver.values().get(0);
+        // Assert: if bigger than 0 and if returns 10 results (only for un-precised search)
         Assert.assertTrue(searchResults.getResponse().getDocs().size() > 0);
+        Assert.assertEquals(10,searchResults.getResponse().getDocs().size());
+
+        for (Doc d: searchResults.getResponse().getDocs()) {
+            Assert.assertEquals( "Politics", d.getNewsDesk());
+        }
     }
+
+    @Test
+    public void isStream_fetchSearchResultsForFalseDate_ReturnNoResults() {
+        Observable<NYTSearch> observableSearchResults = NYTStreams.streamFetchSearchResults("20801220",null,"news_desk:(\"Politics\")","","newest");
+
+        TestObserver<NYTSearch> searchResultTestObserver = new TestObserver<>();
+
+        observableSearchResults.subscribeWith(searchResultTestObserver)
+                .assertNoErrors()
+                .assertNoTimeout()
+                .awaitTerminalEvent();
+
+        NYTSearch searchResults = searchResultTestObserver.values().get(0);
+
+        Assert.assertEquals( true, searchResults.getResponse().getDocs().size() == 0);
+    }
+
+    @Test
+    public void isStream_fetchSearchResultsForBeginDate_returnBeginDateEqualsToOldestTitle() {
+        Observable<NYTSearch> observableSearchResults = NYTStreams.streamFetchSearchResults("20190801", null, "news_desk:(\"Politics\")", "", "oldest");
+
+        TestObserver<NYTSearch> searchResultTestObserver = new TestObserver<>();
+
+        observableSearchResults.subscribeWith(searchResultTestObserver)
+                .assertNoErrors()
+                .assertNoTimeout()
+                .awaitTerminalEvent();
+
+        NYTSearch searchResults = searchResultTestObserver.values().get(0);
+        Doc doc = searchResults.getResponse().getDocs().get(0); // Get first title in position [0] (sort = oldest)
+        String date = convertDate(doc.getPubDate());
+        Assert.assertEquals("20190801", date);
+    }
+
+    //----------------------------------------------------------------------------------------------
+    //Helper methods
+    //----------------------------------------------------------------------------------------------
+    // Date format converter
+    private String convertDate(String inputDate) {
+        DateFormat theDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = null;
+        try {
+            date = theDateFormat.parse(inputDate);
+        } catch (ParseException parseException) {
+            // Date is invalid
+            Log.e(TAG, "convertDate: parse error while converting the Date" );
+        } catch(Exception exception) {
+            // Generic catch.
+            Log.e(TAG, "convertDate: generic error while converting the Date" );
+        }
+        theDateFormat = new SimpleDateFormat("yyyyMMdd");
+
+        return theDateFormat.format(date);
+    }
+
+    // Date modifier to subtract days
+    private static String subtractDays(int days){
+        // get today's date
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+        cal.add(Calendar.DATE, -days);
+        return getTodayDate(cal);
+    }
+    // get today's date
+    public static String getTodayDate(Calendar cal) {
+        return "" + cal.get(Calendar.YEAR) + "0"+
+                (cal.get(Calendar.MONTH) + 1) + cal.get(Calendar.DATE);
+    }
+
 }
