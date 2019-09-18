ebe00655751cf3ad739351cfaf7b5ee98e82ad2b
e210564e <devedelie@gmail.com>
2019-09-18 19:21:34 +0200

    - Excluded build.gradle from .gitIgnore file to avoid uploading it to git - Moved the API_KEY into project:gradle.properties - added a reference to the API_KEY on app:build.gradle within the 'defaultConfig' - changed reference in source code to 'private static final String API_KEY = BuildConfig.API_KEY;'

--- app/src/main/java/com/elbaz/eliran/mynewsapp/models/Constants.java
+++ app/src/main/java/com/elbaz/eliran/mynewsapp/models/Constants.java
@@ -1,12 +1,14 @@
 package com.elbaz.eliran.mynewsapp.models;
 
+import com.elbaz.eliran.mynewsapp.BuildConfig;
+
 /**
  * Created by Eliran Elbaz on 11-Jul-19.
  */
 public class Constants {
 
     // Basic constants
-    public static final String API_KEY = "eUdpsuImhyQRapDx4vkN0NMOJZEYSqYA";
+    public static final String API_KEY = BuildConfig.API_KEY;
     public static final String BASE_URL = "https://api.nytimes.com/";
     public static final String SEPARATOR = " > ";
     public static final String DEVELOPERS_LINK = "https://developer.nytimes.com";
